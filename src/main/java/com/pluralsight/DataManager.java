7c0c30e7d2b6fcbf6b4c8fb912a553eada82647b
Joshua <Joshua32149@gmail.com>
2023-12-14 10:39:47 -0500

    Added Update method and Delete method for DB

--- src/main/java/com/pluralsight/DataManager.java
+++ src/main/java/com/pluralsight/DataManager.java
@@ -25,7 +25,8 @@ public class DataManager {
         String userInput = myScanner.nextLine().trim();
         String[] inputSplit = userInput.split(" ");
 
-        try (Connection conn = dataSource.getConnection(); PreparedStatement queryEx = conn.prepareStatement("SELECT * FROM Shippers");
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement queryEx = conn.prepareStatement("SELECT * FROM Shippers");
              PreparedStatement pStmt = conn.prepareStatement("INSERT INTO Shippers(CompanyName,Phone) VALUES (?,?)")) {
             pStmt.setString(1, inputSplit[0]);
             pStmt.setString(2, inputSplit[1]);
@@ -38,6 +39,7 @@ public class DataManager {
                     System.out.println("----------------------------");
                 }
             }
+            myScanner.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -48,7 +50,8 @@ public class DataManager {
         System.out.println("Hello! Do you want to continue and update the phone number of a shipper? Yes or No ");
         String userChoice = myScanner.nextLine().trim();
         if (userChoice.equalsIgnoreCase("Yes")) {
-            try (Connection conn = dataSource.getConnection(); PreparedStatement queryEx = conn.prepareStatement("SELECT * FROM Shippers")) {
+            try (Connection conn = dataSource.getConnection();
+                 PreparedStatement queryEx = conn.prepareStatement("SELECT * FROM Shippers")) {
                 try (ResultSet rs = queryEx.executeQuery()) {
                     while (rs.next()) {
                         System.out.println("ID: " + rs.getString("ShipperID"));
@@ -59,14 +62,76 @@ public class DataManager {
                 }
 
                 System.out.println("Of the shippers above which would you like to change? Please enter the id and the phone number of the shipper.");
+                String userInput = myScanner.nextLine();
+                String[] userInputSplit = userInput.split(" ");
+                System.out.println("Now please input your new data,the new phone number. ");
+                String newNumber = myScanner.nextLine().trim();
+                try (PreparedStatement updateEx = conn.prepareStatement("UPDATE Shippers SET Phone = ? WHERE ShipperID = ? AND Phone = ?")) {
+                    updateEx.setString(1, newNumber);
+                    updateEx.setString(2, userInputSplit[0]);
+                    updateEx.setString(3, userInputSplit[1]);
+                    int rowsAffected = updateEx.executeUpdate();
+                    if (rowsAffected > 0) {
+                        System.out.println("Here is the updated table.");
+                        System.out.println("Update Successful");
+                    } else {
+                        System.out.println("Nothing was updated. Verify the Shipper ID.");
+                    }
+                    try (ResultSet rs = queryEx.executeQuery()) {
+                        while (rs.next()) {
+                            System.out.println("ID: " + rs.getString("ShipperID"));
+                            System.out.println("Company Name: " + rs.getString("CompanyName"));
+                            System.out.println("Phone: " + rs.getString("Phone"));
+                            System.out.println("----------------------------");
+                        }
+                    }
+                }
+                myScanner.close();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
+
         } else {
             System.out.println("Thank you and have a good day.");
             System.exit(0);
         }
+    }
 
+    public static void deleteShipper() {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement queryEx = conn.prepareStatement("SELECT * FROM Shippers")) {
+            try (ResultSet rs = queryEx.executeQuery()) {
+                while (rs.next()) {
+                    System.out.println("ID: " + rs.getString("ShipperID"));
+                    System.out.println("Company Name: " + rs.getString("CompanyName"));
+                    System.out.println("Phone: " + rs.getString("Phone"));
+                    System.out.println("----------------------------");
+                }
+            }
+            System.out.println("Please insert the ID of the Shipper that you want to delete.");
+            String userInput = myScanner.nextLine().trim();
+            try (PreparedStatement deleteEx = conn.prepareStatement("DELETE FROM Shippers WHERE ShipperID = ?")) {
+                deleteEx.setString(1, userInput);
+                int rowsDeleted = deleteEx.executeUpdate();
+                if (rowsDeleted > 0) {
+                    try (ResultSet rs = queryEx.executeQuery()) {
+                        while (rs.next()) {
+                            System.out.println("ID: " + rs.getString("ShipperID"));
+                            System.out.println("Company Name: " + rs.getString("CompanyName"));
+                            System.out.println("Phone: " + rs.getString("Phone"));
+                            System.out.println("----------------------------");
+                        }
+                        System.out.println("Here is the updated table.");
+                        System.out.println("Delete Successful");
+                    }
+                } else {
+                    System.out.println("Nothing was deleted. Verify the Shipper ID.");
+                }
+                myScanner.close();
 
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,8 +1,30 @@
 package com.pluralsight;
 
+import static com.pluralsight.DataManager.myScanner;
+
 public class Main {
     public static void main(String[] args) {
     DataManager.startConnection();
-    DataManager.newShipper();
+        System.out.println("""
+                Hello User, What would you like to do ?
+                1) Add a new Shipper
+                2) Update a Shipper
+                3) Delete a Shipper
+                0) Exit""");
+        String userChoice = myScanner.nextLine();
+        switch (userChoice){
+            case "1": DataManager.newShipper();
+            break;
+            case "2":
+                DataManager.updateShipper();
+            break;
+            case "3": DataManager.deleteShipper();
+                break;
+            case "0":
+                System.out.println("Thank you and have a good day.");
+                System.exit(0);
+        }
+
+
     }
 }
